f79f4956fb8707a6ed056f9535d3d8be97cd0dee
Gavrilenko <gavrilenko.georgi@gmail.com>
2021-02-11 15:15:38 +0200

    Cleanup

--- src/components/pages/Orders/Orders.jsx
+++ src/components/pages/Orders/Orders.jsx
@@ -6,7 +6,7 @@ import classes from './Orders.module.sass'
 const Orders = () => {
 
 	return <section className={classes.ordersLayout}>
-		<h1>Orders!!</h1>
+		<h1>Orders</h1>
 		<article>
 			<OrdersList />
 		</article>
